84d2901440ba95ec15b1bc92597b3e3cb685463a
IT Ako pero Bobo <164977444+ITArchitect111@users.noreply.github.com>
2025-04-30 21:39:47 +0800

    Update slider.js

--- Assets/JS/slider.js
+++ Assets/JS/slider.js
@@ -1,17 +1,22 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+<script>
+let slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  if (dots.length > 0) {
+    dots[slideIndex - 1].className += " active-dot";
+  }
+  setTimeout(showSlides, 5000); // Change every 5 seconds
 }
-
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-}
+</script>
