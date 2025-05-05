31f84d7332adeb93479c5f4b1a51ec5ab29c6f00
IT Ako pero Bobo <164977444+ITArchitect111@users.noreply.github.com>
2025-05-05 12:47:19 +0800

    Update galaxy.js

--- Assets/JS/galaxy.js
+++ Assets/JS/galaxy.js
@@ -1,8 +1,8 @@
-/* ---- particles.js config ---- */
-particlesJS("tech", {
+​/* ---- particles.js config ---- */
+particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 300,
+      "value": 1000,
       "density": {
         "enable": true,
         "value_area": 1000
@@ -22,6 +22,75 @@ particlesJS("tech", {
       }
     },
     "size": {
-      "value": 5,
+      "value": 7,
       "random": true,
       "anim": {
+        "enable": true,
+        "speed": 1,
+        "size_min": 1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 170,
+      "color": "#58636d",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "bottom",
+      "random": true,
+      "straight": true,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 850,
+        "rotateY": 1740
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 100,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 45,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 150,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
